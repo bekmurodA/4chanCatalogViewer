a34d4fdb2d6da0da63689225b541a4defe98be8f
bekmurodA <7248774american@gmail.com>
2021-05-04 17:24:22 +0500

    now can see single thread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import requests
+import requests,sys
 url ="https://a.4cdn.org/g/catalog.json"
+urlThread="https://a.4cdn.org/g/thread/" #number.json
 def Print_threads():
     response = requests.get(url)
     jresponse = response.json()
@@ -12,8 +13,26 @@ def Print_threads():
             except KeyError:
                 pass
     response.close()
+    
+
+def Print_thread(n):
+    response = requests.get(urlThread+n+".json").json()
+    #print(response['posts'])
+    for keys in response['posts']:
+            try:
+                print(f"{keys['no']}\v{keys['com']}\n")
+                print()
+            except KeyError:
+                pass
 
 if __name__ == '__main__':
-    Print_threads()
+    threads=input("Threads in g(g) or particular thread(t): ")
+    if (threads == 'g'):
+        Print_threads()
+    elif(threads == 't'):
+        thread_number=input("enter the thread number (quit 0) ")
+        if (int(thread_number) == 0):
+            sys.exit()
+        Print_thread(thread_number)
     
 
